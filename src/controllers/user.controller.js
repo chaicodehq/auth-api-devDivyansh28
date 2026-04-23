4fc3ad10543b971289f546b396e93a1997d880f5
DevDivyansh <576divyanshsharma@gmail.com>
2026-04-23 11:13:23 +0530

    Authentication Module done ✅✅

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,7 +8,8 @@ import { User } from '../models/user.model.js';
  */
 export async function listUsers(req, res, next) {
   try {
-    // Your code here
+    const users = await User.find()
+    return res.status(200).json({users});
   } catch (error) {
     next(error);
   }
@@ -24,7 +25,9 @@ export async function listUsers(req, res, next) {
  */
 export async function getUser(req, res, next) {
   try {
-    // Your code here
+    const user  = await User.findById(req.params.id)
+    if(!user) return res.status(404).json({error:{message : "User not found"}});
+    return res.status(200).json({user})
   } catch (error) {
     next(error);
   }
@@ -40,7 +43,10 @@ export async function getUser(req, res, next) {
  */
 export async function deleteUser(req, res, next) {
   try {
-    // Your code here
+    const user = await User.findByIdAndDelete(req.params.id)
+    if(!user) return res.status(404).json({error : {message : "User not found"}});
+
+    return res.status(200).json({message : "User deleted successfully"})
   } catch (error) {
     next(error);
   }
